9923a8ab807fa016cd2273ebcb2935ab5b05e582
torstenbrickley <torstenbrickley@oldrochester.org>
2023-05-09 12:39:11 -0400

    ya boyt

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import word form "/scripts/word";
+
+export default function Landing() {
+
+
+
+
+    return (
+        <div className="flex flex-col items-center min-h-screen gap-16 p-8 text-center md:p-16 md:py-24">
+                <h1 className="text-4xl font-bold text-center text-white">Java</h1>
+                <button id="U1" className="px-4 py-2 text-white bg-blue-500 rounded-lg">Genaret word</button>
+
+
+
+
+</div>
+    )
+
+}
---
+++ scripts/1-8-java-words.ts
@@ -0,0 +1,147 @@
+
+
+const words =
+[
+  {
+    "word": "attribute",
+    "definition": "a characteristic of an object"
+  },
+  {
+    "word": "behavior",
+    "definition": "an action that an object can perform"
+  },
+  {
+    "word": "boolean",
+    "definition": "a primitive data type that can be either true or false"
+  },
+  {
+    "word": "bug",
+    "definition": "an error in the code"
+  },
+  {
+    "word": "class",
+    "definition": "a programmer-defined blueprint from which objects are created"
+  },
+  {
+    "word": "class header",
+    "definition": "consists of the class keyword and the name of the class"
+  },
+  {
+    "word": "condition",
+    "definition": "determines whether or not to execute a block of code"
+  },
+  {
+    "word": "conditional statement",
+    "definition": "a statement that only executes when a condition is true"
+  },
+  {
+    "word": "constructor",
+    "definition": "a block of code that has the same name as the class and tells the computer how to create a new object"
+  },
+  {
+    "word": "constructor signature",
+    "definition": "the first line of the constructor which includes the public keyword, the constructor name, and the values to specify when an object is created"
+  },
+  {
+    "word": "debugging",
+    "definition": "finding and fixing problems in an algorithm or program"
+  },
+  {
+    "word": "dot operator",
+    "definition": "used to call a method in a class"
+  },
+  {
+    "word": "if statement (one-way selection statement)",
+    "definition": "a conditional statement that only executes when the condition is true"
+  },
+  {
+    "word": "inheritance",
+    "definition": "an object-oriented programming principle where a subclass inherits the attributes and behaviors of a superclass"
+  },
+  {
+    "word": "instantiate",
+    "definition": "to call the constructor to create an object"
+  },
+  {
+    "word": "integrated development environment (IDE)",
+    "definition": "a software application for writing, compiling, testing, and debugging program code"
+  },
+  {
+    "word": "logic error",
+    "definition": "an error that occurs when the code runs but does not do what was expected"
+  },
+  {
+    "word": "method",
+    "definition": "a named set of instructions to perform a task"
+  },
+  {
+    "word": "method signature",
+    "definition": "consists of a name and parameter list"
+  },
+  {
+    "word": "object",
+    "definition": "an instance of a class"
+  },
+  {
+    "word": "object-oriented programming",
+    "definition": "an approach to creating and using models of physical or imagined objects"
+  },
+  {
+    "word": "package",
+    "definition": "a collection of similar classes"
+  },
+  {
+    "word": "parameter",
+    "definition": "a variable in the method or constructor signature that defines the type of value to receive when the method or constructor is called"
+  },
+  {
+    "word": "return",
+    "definition": "to exit a method and go back to the point in the program that called it with the requested value or information"
+  },
+  {
+    "word": "software",
+    "definition": "a collection of instructions that is run by a computer"
+  },
+  {
+    "word": "software engineer",
+    "definition": "a person who designs, develops, and tests software for home, school, and business use"
+  },
+  {
+    "word": "source code",
+    "definition": "a collection of programming commands"
+  },
+  {
+    "word": "state",
+    "definition": "the attributes of an object that are represented by its instance variables"
+  },
+  {
+    "word": "subclass",
+    "definition": "a class that extends a superclass and inherits its attributes and behaviors"
+  },
+  {
+    "word": "super keyword",
+    "definition": "a keyword used to refer to the superclass"
+  },
+  {
+    "word": "superclass",
+    "definition": "a class that can be extended to create subclasses"
+  },
+  {
+    "word": "syntax",
+    "definition": "the rules for how a programmer must write code for a computer to understand"
+  },
+  {
+    "word": "syntax error",
+    "definition": "a mistake in the code that does not follow a programming language's syntax"
+  },
+  {
+    "word": "UML diagram",
+    "definition": "a standard way to visualize the design of a program"
+  },
+  {
+    "word": "void method",
+    "definition": "a method that performs an action but does not return a value"
+  }
+]
+
+export default words;
